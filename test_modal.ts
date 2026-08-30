7557128af71ff0e5f5c2bc803063d2ddb9367b75
Foxy <ruebudu69@gmail.com>
2026-08-30 18:13:22 +0200

    chore: remove unused test_modal.ts file

--- test_modal.ts
+++
@@ -1,4 +0,0 @@
-import { ModalBuilder, ActionRowBuilder, TextInputBuilder, StringSelectMenuBuilder } from 'discord.js'
-const modal = new ModalBuilder()
-const sel = new StringSelectMenuBuilder().setCustomId('sel')
-modal.addComponents(new ActionRowBuilder<StringSelectMenuBuilder>().addComponents(sel))
